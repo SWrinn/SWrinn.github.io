7b41dac5874c18d93b621384365e52566a77bb9d
SWrinn <35043165+SWrinn@users.noreply.github.com>
2020-12-02 16:00:55 -0500

    not equal op was wrong

--- siteJS.js
+++ siteJS.js
@@ -240,7 +240,7 @@ function recommendMovie(){
 
     for(i = 0; i < sameUsers.length; i++){
       //get the sim score for each user
-      if(i != currentUser){
+      if(i !== currentUser){
         //call sim, giving i, current and the matrix
         score = sim(currentUser, i, rankMat);
         simScores.push(score);
