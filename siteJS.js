59d022cddda9c11b528b185f4fea0f4e74eb9df1
SWrinn <35043165+SWrinn@users.noreply.github.com>
2020-12-02 13:47:34 -0500

    more debugging

--- siteJS.js
+++ siteJS.js
@@ -224,8 +224,8 @@ function recommendMovie(){
         //make a row to put in the array
         if(userRatings[user].hasOwnProperty(movie)){
           //this user has rated the movie
-          console.log(userRatings[user][movie]);
-          rankRow.push(userRatings[user][movie]);
+          holder = userRatings[user][movie];
+          rankRow.push(holder);
         }else{
           //the user has not rated the movie
           rankRow.push(0);
